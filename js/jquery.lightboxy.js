a14b1db3e0d24aca02df40b8ad8d0f71d8e3f024
menor <josemenor@gmail.com>
2015-01-25 10:57:59 +0100

    Fix jquery link, add plugin structure

--- js/jquery.lightboxy.js
+++ js/jquery.lightboxy.js
@@ -1,6 +1,21 @@
 (function($) {
-  $.fn.lightboxy = function() {
+  $.fn.lightboxy = function(prop){
 
-  }
+    // Default parameters
+    var options = $.extend({
+      height : "250",
+      width : "500",
+      title:"JQuery Lightboxy",
+      description: "A jQuery lightbox plugin built for the sake of it.",
+      top: "20%",
+      left: "30%",
+    },prop);
+
+    return this.click(function(e){
+         alert('Yo!');
+    });
+
+    return this;
+  };
 
 }(jQuery));
