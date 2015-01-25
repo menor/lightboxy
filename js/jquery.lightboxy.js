ff810607f9b917d11e35b4301f6dd0902285c50c
menor <josemenor@gmail.com>
2015-01-25 12:09:29 +0100

    Add styles, but code is not working

--- js/jquery.lightboxy.js
+++ js/jquery.lightboxy.js
@@ -1,5 +1,5 @@
 (function($) {
-  $.fn.lightboxy = function(prop){
+  $.fn.lightboxy = function(prop) {
 
     // Default parameters
     var options = $.extend({
@@ -11,9 +11,35 @@
       left: "30%",
     },prop);
 
-    return this.click(function(e){
-         alert('Yo!');
+    return this.click(function(e) {
+         console.log('one');
+         add_overly();
+         console.log('two');
+         add_styles();
+         console.log('three');
     });
+
+    function add_overly(){
+      var overly = "<div id='overly'></div>"
+      $(overly).appendTo("body");
+      console.log('overly_added');
+    }
+
+    function add_styles() {
+      var viewportHeight = $(document).height();
+      var viewportWidth = $(document).width();
+
+      $("#overly").css({
+        'position': 'absolute',
+        'top': 0,
+        'right': 0,
+        'bottom': 0,
+        'left': 0,
+        'background-color':'rgba(0,0,0,0.6)',
+        'z-index':'10'
+      });
+      console.log('styles_added');
+    }
 
     return this;
   };
